3ac392959fa46a42855ecfe542a0fb85d93948a8
Nishant Garg <nishant2024rde@gmail.com>
2025-12-24 19:30:53 +0530

    chech user exist in route.ts

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,17 @@
+import connectDB from "@/lib/db";
+import { connect } from "http2";
 import { NextRequest } from "next/server";
+import User from "@/models/user.model";
 
 export async function POST(req:NextRequest){
+    try {
+        await connectDB()
+        const {name, email, password} = await req.json()
+        const existUser = await User.findOne({email})
 
+    } catch (error){
+
+    }
 }
+
+
