82cea2f4105877a86683c325a3430701cc5941c7
Nishant Garg <nishant2024rde@gmail.com>
2025-12-24 19:34:56 +0530

    check user and password

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import connectDB from "@/lib/db";
 import { connect } from "http2";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/user.model";
 
 export async function POST(req:NextRequest){
@@ -8,6 +8,19 @@ export async function POST(req:NextRequest){
         await connectDB()
         const {name, email, password} = await req.json()
         const existUser = await User.findOne({email})
+        if(existUser){
+            return NextResponse.json(
+                {message: "email already exists"},
+                {status: 400},
+            )
+        }
+        if(password.legth < 6){
+            return NextResponse.json(
+                {message: "password must be at least 6 characters"},
+                {status: 400},
+            )
+
+        }
 
     } catch (error){
 
