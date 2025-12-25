c8585c18449aabdc2b92924fd4f2147a65b073da
Nishant Garg <nishant2024rde@gmail.com>
2025-12-25 11:07:56 +0530

    auth cred in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import connectDB from "./lib/db";
+import User from "./models/user.model";
 
 export const{handlers, signIn, signOut, auth} = NextAuth({
     providers: [
@@ -8,9 +11,28 @@ export const{handlers, signIn, signOut, auth} = NextAuth({
                 email: {label: "email", type: "email"},
                 password: {label: "password", type: "password"},
             },
-            // async authorize(credentials, request){
+            async authorize(credentials, request){
                 
-            // },
+                    await connectDB();
+                    const email = credentials.email
+                    const password = credentials.password as string
+                    const user = await User.findOne({email})
+                    if(!user){
+                        throw new Error("no user found")
+                    }
+                    const isMatch = await bcrypt.compare(password, user.password)
+                    if(!isMatch){
+                        throw new Error("invalid password")
+                    }
+                    return {
+                        id:user._id,
+                        email: user.email,
+                        name: user.name,
+                        
+
+                    }
+                
+            },
         })
     ],
 })
