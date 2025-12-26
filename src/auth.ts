22b72498c77ecc4dacaf5d85a2dede1f96175d15
Nishant Garg <nishant2024rde@gmail.com>
2025-12-26 09:56:19 +0530

    adding jwt strategy in session

--- src/auth.ts
+++ src/auth.ts
@@ -61,7 +61,12 @@ export const{handlers, signIn, signOut, auth} = NextAuth({
     },
     pages:{
         signIn: "/login",
-    }
+        error: "/login",
+    },
+    session:{
+        strategy: "jwt",
+    },
+    
 })
 
 
