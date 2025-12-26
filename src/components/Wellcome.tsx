5868d319b00689212dcb73727a4c9fec94ab361e
Nishant Garg <nishant2024rde@gmail.com>
2025-12-26 10:33:29 +0530

    testing framer motion

--- src/components/Wellcome.tsx
+++ src/components/Wellcome.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
 function Wellcome() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
-      
+      <motion.div>Hell Here</motion.div>
     </div>
   )
 }
