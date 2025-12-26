349d3b22bfa92ef85679d2869740df9771f01ab6
Nishant Garg <nishant2024rde@gmail.com>
2025-12-26 10:35:41 +0530

    animation test with frammer motion

--- src/components/Wellcome.tsx
+++ src/components/Wellcome.tsx
@@ -3,7 +3,10 @@ import { motion } from 'framer-motion'
 function Wellcome() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
-      <motion.div>Hell Here</motion.div>
+      <motion.div
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      >Hell Here</motion.div>
     </div>
   )
 }
