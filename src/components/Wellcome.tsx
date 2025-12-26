3c0216a92ed6221629a7d4b856599006aade63fd
Nishant Garg <nishant2024rde@gmail.com>
2025-12-26 10:43:42 +0530

    adding duration

--- src/components/Wellcome.tsx
+++ src/components/Wellcome.tsx
@@ -5,8 +5,16 @@ function Wellcome() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
       <motion.div
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      initial={{
+        opacity:0
+      }}
+      animate={{
+        opacity:1
+      }}
+      transition={{
+        duration:2
+      }}
+
       >Hell Here</motion.div>
     </div>
   )
