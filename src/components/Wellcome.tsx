f400935a466cb48b63d5c9ba0600786fc4da0774
Nishant Garg <nishant2024rde@gmail.com>
2025-12-26 10:30:59 +0530

    adding tailwind to wellcome

--- src/components/Wellcome.tsx
+++ src/components/Wellcome.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Wellcome() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
       
     </div>
   )
